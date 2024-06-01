5b9e00b69133ec2a3e399f9514addc0727892307
Christine Wong <jumiryoo@gmail.com>
2024-06-01 00:36:37 -0700

    commented out the public routes due to an error on clerkMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,19 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-    publicRoutes: [
-        '/',
-        '/events/:id',
-        '/api/webhook/clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing'
-      ],
-    ignoredRoutes: [
-        '/api/webhook/clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing'
-    ]
-});
+export default clerkMiddleware(
+    // {publicRoutes: [
+    //     '/',
+    //     '/events/:id',
+    //     '/api/webhook/clerk',
+    //     '/api/webhook/stripe',
+    //     '/api/uploadthing'
+    //   ],
+    // ignoredRoutes: [
+    //     '/api/webhook/clerk',
+    //     '/api/webhook/stripe',
+    //     '/api/uploadthing'
+    // ]}
+    );
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
