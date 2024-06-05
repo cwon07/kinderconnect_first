070c7fb02aa04f808cb95923cf4b3a05d0d2a2cc
Christine Wong <jumiryoo@gmail.com>
2024-06-04 23:25:07 -0700

    added Event Title, Category dropdown menu, and the description box

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -2,8 +2,6 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import { z } from "zod"
-
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -15,6 +13,13 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { eventFormSchema } from "@/lib/validator"
+import * as z from "zod"
+import { eventDefaultValues } from "@/constants"
+import Dropdown from "./Dropdown"
+import { Textarea } from "@/components/ui/textarea"
+import FileUploader from "./FileUploader"
+
  
 
 type EventFormProps = {
@@ -23,32 +28,75 @@ type EventFormProps = {
 }
 
 const EventForm = ({ userId, type }: EventFormProps) => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
+
+  const initialValues = eventDefaultValues;
+
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues
   })
 
   return (
     <Form {...form}>
-    <form onSubmit={form.handleSubmit(onsubmit)} className="space-y-8">
+    <form onSubmit={form.handleSubmit(onsubmit)} className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 md:flex-row">
       <FormField
         control={form.control}
-        name="username"
+        name="title"
         render={({ field }) => (
-          <FormItem>
-            <FormLabel>Username</FormLabel>
+          <FormItem className="w-full">
             <FormControl>
-              <Input placeholder="shadcn" {...field} />
+              <Input placeholder="Event Title" {...field} className="input-field"/>
             </FormControl>
-            <FormDescription>
-              This is your public display name.
-            </FormDescription>
             <FormMessage />
           </FormItem>
         )}
       />
+      <FormField
+    control={form.control}
+    name="categoryId"
+    render={({ field }) => (
+      <FormItem className="w-full">
+        <FormControl>
+          <Dropdown onChangeHandler={field.onChange} value={field.value} />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+    </div>
+
+    <div className="flex flex-col gap-5 md:flex-row">
+    <FormField
+    control={form.control}
+    name="description"
+    render={({ field }) => (
+      <FormItem className="w-full">
+        <FormControl className="h-72">
+          <Textarea placeholder="Description" {...field} className="textarea rounded-2xl" />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+   <FormField
+    control={form.control}
+    name="imageUrl"
+    render={({ field }) => (
+      <FormItem className="w-full">
+        <FormControl className="h-72">
+          <FileUploader
+            onFieldChange={field.onChange}
+            imageUrl={field.value}
+            setFiles={setFiles}
+          />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+    </div>
+
       <Button type="submit">Submit</Button>
     </form>
   </Form>
