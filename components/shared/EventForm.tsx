db4ec42955dfe26d970fe12df0b4951ddcd4dbfb
Christine Wong <jumiryoo@gmail.com>
2024-06-04 22:21:40 -0700

    created eventFormSchema

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,16 +1,57 @@
 'use client'
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+ 
+
 type EventFormProps = {
   userId: string
   type: "Create" | "Update"
 }
 
 const EventForm = ({ userId, type }: EventFormProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
 
   return (
-    <div>
-      EventForm{type}
-    </div>
+    <Form {...form}>
+    <form onSubmit={form.handleSubmit(onsubmit)} className="space-y-8">
+      <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input placeholder="shadcn" {...field} />
+            </FormControl>
+            <FormDescription>
+              This is your public display name.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  </Form>
   )
 }
 
