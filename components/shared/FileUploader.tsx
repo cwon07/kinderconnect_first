00047bd0f8d2ba3a077412a82afe06a7a8596224
Christine Wong <jumiryoo@gmail.com>
2024-06-10 21:34:00 -0700

    added uploadthing for the event form

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,4 +1,11 @@
-import { Dispatch, SetStateAction, useState, useCallback } from "react"
+'use client'
+
+import { Dispatch, SetStateAction, useCallback } from "react"
+import { useDropzone } from "@uploadthing/react";
+import { generateClientDropzoneAccept } from "uploadthing/client"
+import { FileWithPath } from '@uploadthing/react'
+import { convertFileToUrl } from "@/lib/utils";
+import { Button } from "../ui/button";
 
 type FileUploadProps = {
     imageUrl: string
@@ -6,62 +13,42 @@ type FileUploadProps = {
     setFiles: Dispatch<SetStateAction<File[]>>
 }
 
-// Note: `useUploadThing` is IMPORTED FROM YOUR CODEBASE using the `generateReactHelpers` function
-import { useDropzone } from "@uploadthing/react";
-import { generateClientDropzoneAccept } from "uploadthing/client";
- 
-import { useUploadThing } from "@/lib/uploadthing";
- 
-export function MultiUploader() {
-  const [files, setFiles] = useState<File[]>([]);
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    setFiles(acceptedFiles);
+export function FileUploader ({ imageUrl, onFieldChange, setFiles} : FileUploadProps) {
+  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+    setFiles(acceptedFiles)
+    onFieldChange(convertFileToUrl(acceptedFiles[0]))
   }, []);
- 
-  const { startUpload, permittedFileInfo } = useUploadThing(
-    "myUploadEndpoint",
-    {
-      onClientUploadComplete: () => {
-        alert("uploaded successfully!");
-      },
-      onUploadError: () => {
-        alert("error occurred while uploading");
-      },
-      onUploadBegin: () => {
-        alert("upload has begun");
-      },
-    },
-  );
- 
-  const fileTypes = permittedFileInfo?.config
-    ? Object.keys(permittedFileInfo?.config)
-    : [];
  
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    accept: fileTypes ? generateClientDropzoneAccept(fileTypes) : undefined,
+    accept: 'image/*' ? generateClientDropzoneAccept(['image/*']) : undefined,
   });
  
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <div>
-        {files.length > 0 && (
-          <button onClick={() => startUpload(files)}>
-            Upload {files.length} files
-          </button>
-        )}
-      </div>
-      Drop files here!
-    </div>
-  );
-}
-
-const FileUploader = ({ imageUrl, onFieldChange, setFiles}:
-    FileUploadProps) => {
-        return (
-            <div>FileUploader</div>
-        )
-    }
-
-export default FileUploader
+    <div {...getRootProps()}
+        className="flex-center bg-dark-3 flex h-72 cursor-pointer flex-col overflow-hidden rounded-xl bg-grey-50">
+      <input {...getInputProps()} className="cursor-pointer"/>
+      
+      {imageUrl ? (
+        <div className="flex h-full w-full flex-1 justify-center">
+            <img
+                src={imageUrl}
+                alt="image"
+                width={250}
+                height={250}
+                className="w-full object-cover object-center"
+            />
+        </div>
+      ) : (
+        <div className="flex-center flex-col py-5 text-grey-500">
+            <img src="/assets/icons/upload.svg" width={77} height={77} alt="file upload" />
+            <h3 className="mb-2 mt-2">Drag photo here</h3>
+            <p className="p-medium-12 mb-4">SVG, PNG, JPG</p>
+            <Button type="button" className="rounded-full">
+                Select from computer
+            </Button>
+        </div>    
+  )}
+  </div>
+  )
+      }
