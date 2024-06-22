41e02fcca0101ec34299aa0c6c1f3c009e80762d
Christine Wong <jumiryoo@gmail.com>
2024-06-21 23:41:56 -0700

    edited user.model.ts

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,9 +1,18 @@
-import { Schema, model, models } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
-const UserSchema = new Schema({
+export interface UserProps extends Document {
+  clerkId: string;
+  email: string;
+  userName: string;
+  firstName: string | null;
+  lastName: string | null;
+  photo: string
+}
+
+const UserSchema = new Schema<UserProps>({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
+  userName: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: {type: String, required: true },
   photo: { type: String, required: true },
